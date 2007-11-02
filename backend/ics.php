fd7c4bf39df9367beb3445e695d9a71587e80f42
sharky <sharky@b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5>
2007-11-02 12:52:26 +0000

    Fix undefined flag in ICS
    git-svn-id: http://svn.berlios.de/svnroot/repos/z-push/trunk@10 b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5

--- backend/ics.php
+++ backend/ics.php
@@ -503,7 +503,7 @@ class ImportContentsChangesICS extends MAPIMapping {
     // Import a deletion. This may conflict if the local object has been modified.
     function ImportMessageDeletion($objid) {
         // do a 'soft' delete so people can un-delete if necessary
-        mapi_importcontentschanges_importmessagedeletion($this->importer, SYNC_SOFT_DELETE, array(hex2bin($objid)));
+        mapi_importcontentschanges_importmessagedeletion($this->importer, 1, array(hex2bin($objid)));
     }
     
     // Import a change in 'read' flags .. This can never conflict
