8b364e7363d908ed796752d33e3db12bd6f08ca6
skummer <skummer@b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5>
2008-03-26 08:27:42 +0000

    fixed error message on first "ping" when previous ping-defaults not yet exist
    git-svn-id: http://svn.berlios.de/svnroot/repos/z-push/trunk@63 b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5

--- request.php
+++ request.php
@@ -794,16 +794,17 @@ function HandlePing($backend, $devid) {
     $decoder = new WBXMLDecoder($input, $zpushdtd);
     $encoder = new WBXMLEncoder($output, $zpushdtd);
 
-    // Get previous defaults
-    $ping = unserialize(file_get_contents(BASE_PATH . STATE_DIR . "/" . $devid));
-    $collections = $ping["collections"];
-    $lifetime = $ping["lifetime"];
-    
-    if(!isset($collections))
-        $collections = array();
-    if(!isset($lifetime))
-        $lifetime = 0;
-        
+    $collections = array();
+    $lifetime = 0;
+
+    // Get previous defaults if they exist
+    $file = BASE_PATH . STATE_DIR . "/" . $devid;
+    if (file_exists($file)) {
+    	$ping = unserialize(file_get_contents($file));
+	    $collections = $ping["collections"];
+	    $lifetime = $ping["lifetime"];
+    } 
+            
     if($decoder->getElementStartTag(SYNC_PING_PING)) {
         debugLog("Ping init");
         if($decoder->getElementStartTag(SYNC_PING_LIFETIME)) {
