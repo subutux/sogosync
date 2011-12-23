ed2e9e0a88e42396a87b23a4c0e6d9cdee679b20
mku <mku@b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5>
2011-12-23 16:30:57 +0000

    - changed: Increase ping timeout by default (Mantis #536)
    git-svn-id: http://svn.berlios.de/svnroot/repos/z-push/trunk@958 b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5

--- request.php
+++ request.php
@@ -919,7 +919,7 @@ function HandleGetAttachment($backend, $protocolversion) {
 function HandlePing($backend, $devid) {
     global $zpushdtd, $input, $output;
     global $user, $auth_pw;
-    $timeout = (defined('PING_INTERVAL') && PING_INTERVAL > 0) ? PING_INTERVAL : 10;
+    $timeout = (defined('PING_INTERVAL') && PING_INTERVAL > 0) ? PING_INTERVAL : 30;
 
     debugLog("Ping received");
 
